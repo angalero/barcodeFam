8e054a54b84831fec2969058947a210faa588998
angalero <agidaga@gmail.com>
2024-04-18 18:57:01 -0400

    alert for check

--- functions.js
+++ functions.js
@@ -1,9 +1,22 @@
 
 var codeOne = "687HYFD5";
 
+//
+
 
 function getURL() {
-document.getElementById("barcode").innerHTML = 
-        "The full URL of this page is:<br>" + window.location.href;
+    var barcode = document.getElementById("barcode").value;
+
+var code = document.forms["codeInput"]["barcode"].value;
+var codeS = new String(code);
+//alert(barcode);
+
+if (barcode == "COV0047777") {
+    //return <link rel="stylesheet" href="colours\green.css"></link>;
+    alert("works");
+}
+
+
+// "The full URL of this page is:<br>" + window.location.href;
 
 }

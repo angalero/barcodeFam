823912766e06f129071c843455180883fc10f591
angalero <agidaga@gmail.com>
2024-04-20 18:59:13 -0400

    change in function

--- functions.js
+++ functions.js
@@ -3,25 +3,34 @@ var codeOne = "687HYFD5";
 
 //
 
-
-function getURL() {
-    var barcode = document.getElementById("barcode").value;
+function getURL(barcode) {
+   /* var barcode = document.getElementById("barcode").value;
 
 var code = document.forms["codeInput"]["barcode"].value;
-var codeS = new String(code);
-//alert(barcode);
+var codeS = new String(code);*/
+alert(barcode);
 
 if (barcode == "COV0047777") {
     //var currlnk = document.getElementById("colour").innerHTML;
     //var lnk = document.getElementById("colour").innerHTML = "<link rel=stylesheet href=colours\\green.css>";
 
+    alert(barcode);
     document.getElementById("colour").innerHTML = "<link rel=stylesheet href=colours\\green.css>";
     
    // document.getElementById("colour").innerHTML = "<a href=colourGreen.html></a>";
 }
+
+
+
+    alert(val);
+
 
 document.getElementById("test").innerHTML = "test done";
 //alert(currlnk);
 // "The full URL of this page is:<br>" + window.location.href;
 
 }
+
+function myFunction(val) {
+    alert("The input value has changed. The new value is: " + val);
+  }

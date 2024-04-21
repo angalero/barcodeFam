e00a9033296cf4ef1c70e118bf7f25b647715e94
angalero <agidaga@gmail.com>
2024-04-20 22:05:33 -0400

    test change in colours with vercel

--- functions.js
+++ functions.js
@@ -3,31 +3,44 @@ var codeOne = "687HYFD5";
 
 //
 
-function getURL(barcode) {
-   /* var barcode = document.getElementById("barcode").value;
+// verify and change colour
+function getURL() {
+    var barcode = document.getElementById("barcode").value;
 
-var code = document.forms["codeInput"]["barcode"].value;
+/*var code = document.forms["codeInput"]["barcode"].value;
 var codeS = new String(code);*/
-alert(barcode);
+//alert(barcode);
 
 if (barcode == "COV0047777") {
     //var currlnk = document.getElementById("colour").innerHTML;
     //var lnk = document.getElementById("colour").innerHTML = "<link rel=stylesheet href=colours\\green.css>";
 
-    alert(barcode);
+    //alert(barcode);
     document.getElementById("colour").innerHTML = "<link rel=stylesheet href=colours\\green.css>";
     
    // document.getElementById("colour").innerHTML = "<a href=colourGreen.html></a>";
 }
 
 
-
-    alert(val);
+setTimeout(colourChange, 3250) 
 
 
 document.getElementById("test").innerHTML = "test done";
 //alert(currlnk);
 // "The full URL of this page is:<br>" + window.location.href;
+
+}
+
+
+// change back to blank
+
+
+// time back out to blank
+function colourChange() {
+
+    document.getElementById("colour").innerHTML = "";
+    document.getElementById("barcode").value = "";
+
 
 }
 
